091e4bfdbeabf51d2ad5ca3e9a7644c33c2ee7a8
freethewhat <mcsutton@outlook.com>
2017-09-30 16:11:37 -0600

    Created my fade and move stuff

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-
+$(document).ready(function($) {
+   $(":checkbox").change(function(event) {
+     let element = $(this).parents("li");
+     if($(this).prop("checked")){
+       element.fadeOut("slow", function() {
+           $("#completed").append(element);
+           element.fadeIn("slow");
+           element.removeClass("open");
+           element.addClass("closed");
+       })
+     } else if(!$(this).prop("checked")) {
+       element.fadeOut("slow", function() {
+           $("#todo").append(element);
+           element.fadeIn("slow");
+           element.removeClass("closed");
+           element.addClass("open");
+       })
+     }
+   });
+ });
