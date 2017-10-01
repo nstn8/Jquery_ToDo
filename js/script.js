dcd4c7d9b3e1bf5b38ac0846ba188d4570e99b72
Nathan Talley <nathantalley@android-2b8e8215780bf255.localdomain>
2017-10-01 17:05:33 -0600

    paper  boy

--- js/script.js
+++ js/script.js
@@ -15,3 +15,26 @@ $(document).ready(function(){
     };
   })
 })
+var lines = 50;
+var count = 100;
+var inc = 20;
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  stroke(color(0, 50, 200, 90));
+  for (i = 0; i < lines; i++) {
+    line(0, count, width, count);
+    count = count + inc;
+  }
+  stroke(color(200, 0, 0, 100));
+  line(100, 0, 100, height);
+    /*line(0, 120, width, 120);
+    line(0, 140, width, 140);
+    line(0, 160, width, 160);
+    line(0, 180, width, 180);
+    line(0, 200, width, 200);
+    line(0, 220, width, 220);
+    line(0, 240, width, 240);
+    line(0, 260, width, 260);
+    line(0, 280, width, 280);
+    line(0, 300, width, 300);*/
+}
